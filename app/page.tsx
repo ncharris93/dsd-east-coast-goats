1de50b66152dd5391d7771ec410469ca93cbdbcb
Walter Furrer <walterfurrer.dev@gmail.com>
2025-07-21 18:58:03 -0400

    Added placeholder content to landing page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <main></main>
+  return (
+    <main>
+      <h1>Welcome to the DSD East Coast Goats!</h1>
+      <p>Explore our site to learn more about our goats and farm.</p>
+    </main>
+  )
 }
