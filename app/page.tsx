ca2f4cacb21346dff23a51958fe32ddc3e902cd9
Walter Furrer <walterfurrer.dev@gmail.com>
2025-07-21 19:58:29 -0400

    feat: update homepage layout and add hero image

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,28 @@
+import { CalendarHeart } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
     <main>
-      <h1>Welcome to the DSD East Coast Goats!</h1>
-      <p>Explore our site to learn more about our goats and farm.</p>
+      <section className="bg-[url(/images/hero-image.webp)] bg-center h-screen">
+        <div className="flex flex-col gap-8 items-center justify-center h-full backdrop-blur-xs backdrop-brightness-65 text-center text-white">
+          <div>
+            <h1 className="text-4xl font-bold">Welcome to Haven Health</h1>
+            <p className="text-2xl">Your Wellness, Our Commitment</p>
+          </div>
+          <div>
+            <Button
+              variant="default"
+              size="lg"
+              className="text-xl font-semibold"
+            >
+              <CalendarHeart className="size-6" />
+              Schedule Appointment
+            </Button>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
