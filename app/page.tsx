3f3315f35de695657042a90824c22883ce9b8a69
Walter Furrer <walterfurrer.dev@gmail.com>
2025-07-21 18:57:35 -0400

    removed template favicon and images from landing page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,3 @@
-import { hasEnvVars } from '@/lib/utils'
-
 export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center">
-      <p>
-        {hasEnvVars
-          ? 'Environment variables are set. Ready to code!'
-          : 'Environment variables are not set.'}
-      </p>
-    </main>
-  )
+  return <main></main>
 }
