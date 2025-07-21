0b029cf1282735955f73392987d6d93092388b90
Walter Furrer <walterfurrer.dev@gmail.com>
2025-07-21 19:03:01 -0400

    Replaced title and page description in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'Haven Health',
+  description: 'Your Wellness, Our Committment',
 }
 
 const inter = Inter({
