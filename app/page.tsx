ef0f7976ef62d3cee95c0e46b5847af1e3f80d66
Walter Furrer <walterfurrer.dev@gmail.com>
2025-07-25 21:51:40 -0400

    feat(page): enhance layout and styling of home page sections for improved user experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,38 @@
 import {
   ArrowDownCircle,
   CalendarHeart,
-  Clock,
-  Footprints,
   HandHeart,
-  MapPin,
-  Phone,
   PillBottle,
   Stethoscope,
   Syringe,
 } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 import { Button } from '@/components/ui/button'
 
+import AboutImage from '../public/images/about-image.webp'
+
 export default function Home() {
   return (
-    <main className="flex flex-col gap-16 w-full mb-16">
+    <main className="flex flex-col gap-32 w-full mb-32">
       <section
         id="hero"
-        className="bg-[url(/images/hero-image.webp)] bg-center h-screen text-[#F8F6F1] dark:text-foreground"
+        className="bg-[url(/images/hero-image.webp)] bg-center bg-cover h-screen text-[#F8F6F1] dark:text-foreground"
       >
-        <div className="flex flex-col gap-8 items-center justify-center h-full backdrop-blur-xs backdrop-brightness-65 text-center px-4 ">
+        <div className="flex flex-col gap-8 items-center justify-center h-full backdrop-blur-sm backdrop-brightness-60 text-center px-4 2xl:gap-16">
           <div className="flex flex-col gap-2">
-            <h1 className="text-4xl font-bold text-balance">
+            <h1 className="text-4xl font-bold text-balance 2xl:text-5xl">
               Welcome to Haven Health
             </h1>
-            <p className="text-2xl text-balance">
+            <p className="text-2xl text-balance 2xl:text-3xl">
               Your Wellness, Our Commitment
             </p>
           </div>
           <div>
-            <Button
-              variant="default"
-              size="lg"
-              className="text-xl font-semibold"
-            >
+            <Button variant="cta" size="cta" className="text-lg">
               <CalendarHeart className="size-6" />
-              Schedule Appointment
+              <Link href="/appointment">Schedule Appointment</Link>
             </Button>
           </div>
         </div>
@@ -45,21 +41,25 @@ export default function Home() {
             href="#services"
             className="absolute left-1/2 bottom-4 transform -translate-x-1/2"
           >
-            <ArrowDownCircle className="size-16 motion-safe:animate-bounce motion-safe:scroll-smooth" />
+            <ArrowDownCircle className="size-16 motion-safe:animate-bounce hover:text-secondar" />
           </a>
         </div>
       </section>
-      <section id="services" className="flex flex-col gap-16 px-4">
+      <section
+        id="services"
+        className="flex flex-col gap-16 px-4 max-w-screen-xl mx-auto scroll-mt-16"
+      >
         <h2 className="text-3xl font-bold text-center">Our Services</h2>
         <div
           id="services-card-container"
-          className="grid grid-cols-1 gap-x-4 gap-y-8 mx-auto min-w-screen-sm md:grid-cols-2 md:max-w-screen-md xl:grid-cols-4 xl:max-w-screen-xl"
+          className="grid grid-cols-1 gap-x-4 gap-y-8 w-full md:grid-cols-2 md:max-w-screen-md xl:grid-cols-4 xl:max-w-screen-xl mx-auto"
         >
+          {/* Service Cards */}
           <article
             id="check-up"
-            className="bg-card p-3 rounded-lg flex gap-4 items-center"
+            className="bg-card p-4 rounded-lg flex gap-4 items-center shadow-lg"
           >
-            <div className="bg-accent p-3 rounded-full size-fit">
+            <div className="bg-accent p-3 rounded-full flex items-center justify-center">
               <Stethoscope className="size-8 text-accent-foreground" />
             </div>
             <div className="flex flex-col gap-2 w-full">
@@ -71,10 +71,10 @@ export default function Home() {
             </div>
           </article>
           <article
-            id="chronin-condition"
-            className="bg-card p-4 rounded-lg flex gap-4 items-center w-full"
+            id="chronic-condition"
+            className="bg-card p-4 rounded-lg flex gap-4 items-center shadow-lg"
           >
-            <div className="bg-accent p-3 rounded-full size-fit">
+            <div className="bg-accent p-3 rounded-full flex items-center justify-center">
               <PillBottle className="size-8 text-accent-foreground" />
             </div>
             <div className="flex flex-col gap-2 w-full">
@@ -87,9 +87,9 @@ export default function Home() {
           </article>
           <article
             id="vaccinations"
-            className="bg-card p-4 rounded-lg flex gap-4 items-center w/full"
+            className="bg-card p-4 rounded-lg flex gap-4 items-center shadow-lg"
           >
-            <div className="bg-accent p-3 rounded-full size-fit">
+            <div className="bg-accent p-3 rounded-full flex items-center justify-center">
               <Syringe className="size-8 text-accent-foreground" />
             </div>
             <div className="flex flex-col gap-2 w-full">
@@ -101,9 +101,9 @@ export default function Home() {
           </article>
           <article
             id="mental-health"
-            className="bg-card p-4 rounded-lg flex gap-4 items-center w-full"
+            className="bg-card p-4 rounded-lg flex gap-4 items-center shadow-lg"
           >
-            <div className="bg-accent p-3 rounded-full size-fit">
+            <div className="bg-accent p-3 rounded-full flex items-center justify-center">
               <HandHeart className="size-8 text-accent-foreground" />
             </div>
             <div className="flex flex-col gap-2 w-full">
@@ -115,102 +115,67 @@ export default function Home() {
           </article>
         </div>
         <div className="flex justify-center">
-          <Button variant="default" size="lg" className="text-xl font-semibold">
+          <Button variant="cta" size="cta" className="text-lg">
             <CalendarHeart className="size-6" />
-            Schedule Appointment
+            <Link href="/appointment">Schedule Appointment</Link>
           </Button>
         </div>
       </section>
-      <section
-        id="about"
-        className="flex flex-col gap-16 px-4 items-center max-w-screen-lg mx-auto "
-      >
-        <div className="bg-card text-card-foreground rounded-3xl px-8 py-12 flex flex-col gap-16">
-          <div>
-            <h2 className="text-3xl font-bold text-center">
-              About Haven Health
-            </h2>
-          </div>
-          <div className="grid grid-cols-1 gap-8 text-lg lg:grid-cols-2">
-            <div className="flex flex-col gap-4">
-              <p className="text-pretty">
-                At Haven Health, we are dedicated to providing comprehensive and
-                compassionate healthcare services to our community. Our team of
-                experienced professionals is committed to your well-being,
-                offering personalized care tailored to your unique needs.
-              </p>
-              <p className="text-pretty">
-                From routine check-ups to specialized treatments, we strive to
-                create a welcoming environment where you can feel safe and cared
-                for. Your health is our priority, and we are here to support you
-                every step of the way.
-              </p>
-            </div>
-            <div className="flex flex-col gap-4">
-              <p className="text-pretty">
-                Our state-of-the-art facility is equipped with the latest
-                medical technology, ensuring that you receive the highest
-                quality of care. We believe in a holistic approach to health,
-                focusing not only on physical well-being but also on mental and
-                emotional health.
-              </p>
-              <p className="text-pretty">
-                Join us at Haven Health, where your health journey begins with
-                trust, compassion, and excellence.
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section
-        id="contact"
-        className="flex flex-col gap-16 bg-card text-card-foreground rounded-3xl px-4 max-w-screen-lg mx-auto"
-      >
-        <div className="bg-card text-card-foreground rounded-3xl px-8 py-12 flex flex-col gap-16">
-          <div>
-            <h2 className="text-3xl font-bold text-center">Contact Us</h2>
-          </div>
-          <div className="grid grid-cols-1 gap-y-8 md:grid-cols-3">
-            <div className="flex flex-col gap-4 mx-auto px-4 md:col-span-1">
-              <div className="flex flex-row gap-4">
-                <MapPin className="size-6" />
-                <a
-                  href="https://maps.app.goo.gl/akyyouqaHcVawgiR6"
-                  target="_blank"
-                  rel="noopener, noreferrer"
-                  className="hover:underline"
-                >
-                  <p className="text-pretty">123 Main St., Islip, NY 11751</p>
-                </a>
+      <section id="about" className="max-w-screen-xl px-4 mx-auto">
+        <div className="flex flex-col gap-16 p-8 mx-auto bg-card rounded-3xl shadow-2xl">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-x-4 justify-center text-pretty">
+            {/* Text content */}
+            <div className="grid gap-8 place-items-center lg:col-span-2 order-1 xl:order-1">
+              <h2 className="text-3xl font-bold text-center mb-4">
+                About Haven Health
+              </h2>
+              <div className="flex flex-col gap-4 mb-6">
+                <p>
+                  At Haven Health, we believe that everyone deserves access to
+                  compassionate and comprehensive healthcare. We&apos;re a local
+                  community health clinic dedicated to serving the well-being of
+                  our neighbors.{' '}
+                  <strong>&quot;Your Health, Our Commitment&quot;</strong>{' '}
+                  isn&apos;t just our slogan; it&apos;s the guiding principle
+                  behind everything we do.
+                </p>
+                <p>
+                  We understand that navigating your health journey can be
+                  complex, and our team of experienced and caring professionals
+                  is here to support you every step of the way.
+                </p>
+                <p>
+                  At Haven Health, you&apos;re more than just a patient;
+                  you&apos;re a valued member of our community. We&apos;re
+                  committed to fostering a welcoming and inclusive environment
+                  where you feel heard, respected, and empowered to make
+                  informed decisions about your health. We look forward to
+                  partnering with you on your path to a healthier, happier life.
+                </p>
               </div>
-              <div className="flex flex-row gap-4">
-                <Phone className="size-6" />
-                <a href="tel:6315555555" className="hover:underline">
-                  <p className="text-pretty">(631) 555-5555</p>
-                </a>
-              </div>
-              <div className="flex flex-row gap-4">
-                <Clock className="size-6" />
-                <div className="flex flex-col">
-                  <p className="text-pretty">Monday - Saturday</p>
-                  <p className="text-pretty">9AM - 5PM</p>
-                </div>
-              </div>
-              <div className="flex flex-row gap-2">
-                <Footprints className="size-6" />
-                <div className="flex flex-col">
-                  <p className="text-pretty">Walk-Ins</p>
-                  <p className="text-pretty">9AM - 12PM</p>
-                </div>
-              </div>
-            </div>
-            <div id="map" className="md:col-span-2 md:px-4">
+              {/* At xl, show the map under the text again */}
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12093.524681257835!2d-73.23059610218785!3d40.73163749225007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e8338c078e3efb%3A0xc616688e9560cfd0!2sIslip%2C%20NY!5e0!3m2!1sen!2sus!4v1753375360833!5m2!1sen!2sus"
-                className="w-full aspect-square md:aspect-2/1"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12096.947849051126!2d-73.21755685!3d40.71279975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e8338c078e3efb%3A0xc616688e9560cfd0!2sIslip%2C%20NY!5e0!3m2!1sen!2sus!4v1753486635993!5m2!1sen!2sus"
+                className="w-full rounded-xl aspect-square md:aspect-auto hidden xl:block"
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
               ></iframe>
+            </div>
+            {/* Map at lg only, hidden at xl */}
+            <div className="flex items-center justify-center order-2 xl:order-3 h-full min-h-64">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12096.947849051126!2d-73.21755685!3d40.71279975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e8338c078e3efb%3A0xc616688e9560cfd0!2sIslip%2C%20NY!5e0!3m2!1sen!2sus!4v1753486635993!5m2!1sen!2sus"
+                className="w-full rounded-xl aspect-square md:aspect-auto block xl:hidden h-full min-h-64"
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+              {/* At xl, show the image */}
+              <Image
+                src={AboutImage}
+                alt="A Nurse and their patient"
+                placeholder="blur"
+                className="hidden rounded-xl xl:inline-grid xl:col-span-1"
+              />
             </div>
           </div>
         </div>
