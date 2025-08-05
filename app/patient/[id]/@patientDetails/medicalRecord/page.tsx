90840fb274e39674fa003de626bec48952225120
petertasca <tascapeter@gmail.com>
2025-08-05 14:53:51 -0400

    refactor(medicalrecord page): removed divs from page
    removed excess divs from page

--- app/patient/[id]/@patientDetails/medicalRecord/page.tsx
+++ app/patient/[id]/@patientDetails/medicalRecord/page.tsx
@@ -29,42 +29,38 @@ export default function Page() {
 
   return (
     <section className="border-3 border-black p-2 flex flex-col items-center">
-      <Card className="w-full max-w-screen-lg mx-auto sm:p-6 md:p-8">
-        <div className="border-3 border-lime-300 flex flex-col justify-center">
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="text-xl">
-                Today&apos;s Visit
-              </BreadcrumbItem>
-              <BreadcrumbSeparator>
-                <SlashIcon></SlashIcon>
-              </BreadcrumbSeparator>
-              <BreadcrumbItem className="font-bold text-xl">
-                Medical Record
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-          <div className="flex flex-col border-3  border-black-300">
-            <CardHeader className="font-bold text-xl sm:text-2xl md:text-3xl">
-              <CardTitle>Medical Record</CardTitle>
-            </CardHeader>
-            <CardContent>
-              {medicalVisit?.prescriptions && (
-                <MedicalAccordion
-                  data={medicalVisit?.prescriptions}
-                  label="prescriptions"
-                />
-              )}
-              {medicalVisit?.allergies && (
-                <MedicalAccordion
-                  data={medicalVisit?.allergies}
-                  label="allergies"
-                />
-              )}
-            </CardContent>
-          </div>
-          <div className="flex flex-col border-3  border-black-300"></div>
-        </div>
+      <Card className="w-full max-w-screen-lg mx-auto sm:p-6 md:p-8 border-3 border-lime-300 flex flex-col justify-center">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem className="text-xl">
+              Today&apos;s Visit
+            </BreadcrumbItem>
+            <BreadcrumbSeparator>
+              <SlashIcon></SlashIcon>
+            </BreadcrumbSeparator>
+            <BreadcrumbItem className="font-bold text-xl">
+              Medical Record
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+        <CardHeader className="font-bold text-xl sm:text-2xl md:text-3xl">
+          <CardTitle>Medical Record</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-col border-3  border-black-300">
+          {medicalVisit?.prescriptions && (
+            <MedicalAccordion
+              data={medicalVisit?.prescriptions}
+              label="prescriptions"
+            />
+          )}
+          {medicalVisit?.allergies && (
+            <MedicalAccordion
+              data={medicalVisit?.allergies}
+              label="allergies"
+            />
+          )}
+        </CardContent>
+        <div className="flex flex-col border-3  border-black-300"></div>
       </Card>
     </section>
   )
--- components/patient/MedicalAccordion.tsx
+++ components/patient/MedicalAccordion.tsx
@@ -25,11 +25,11 @@ export default function MedicalAccordion({
         value={label}
         className="flex flex-col  border-b-1 border-destructive"
       >
-        <AccordionTrigger className="flex items-center w-full justify-between p-2">
-          <span className="inline hover:border-b hover:border-destructive">
+        <AccordionTrigger className="flex items-center w-full justify-between p-2 ">
+          <span className="inline hover:border-b hover:border-destructive cursor-pointer">
             {label.charAt(0).toUpperCase() + label.slice(1)}
           </span>
-          <ChevronDown size={20} />
+          <ChevronDown size={20} className="cursor-pointer" />
         </AccordionTrigger>
         <AccordionContent className="overflow-hidden data-[state=open]:animate-accordion-down data-[state=closed]:animate-accordion-up p-2">
           <ul>
